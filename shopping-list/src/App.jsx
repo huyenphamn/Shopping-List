8d18b04fb5392bafefef0753493b803d6135f89b
Heather Pham <phamngochuyen1911@gmail.com>
2024-07-30 09:03:48 +0700

    Add Initial Shopping list interface

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -1,6 +1,7 @@
 
 import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
+import { ShoppingList } from './components/Shopping List/ShoppingList'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className={styles.App}>
       <Navbar/>
+      <ShoppingList/>
     </div>
   )
 }
